3aa93377606ca07e48847337430e88f08eb2244b
Leo Taillon <leotaillon2@gmail.com>
2023-01-04 12:03:22 -0500

    Added Business Screen

---
+++ flutter_application_1/lib/business_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class BusinessScreen extends StatelessWidget {
+  const BusinessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('I am a good worker'),
+    );
+  }
+}
