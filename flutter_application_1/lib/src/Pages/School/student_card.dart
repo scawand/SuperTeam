4e993b7da8258d997ff611f10d8358217f2a7d28
Michaël Rondeau <rondeau_6@hotmail.com>
2023-01-08 13:31:03 -0500

    test rebase

---
+++ flutter_application_1/lib/src/Pages/School/student_card.dart
@@ -0,0 +1,41 @@
+      
+      
+      
+      
+
+
+
+
+
+// AJFIUOHFJUKEHSJIDF HJKHFJKHJKDHJKHJK:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      
+      
+      // Card(
+      //   child: CheckboxListTile(
+      //     title: Text("Michael"),
+      //     value: _checked,
+      //     onChanged: (newValue) {
+      //       setState(() {
+      //         _checked = newValue!;
+      //       });
+      //     },
+      //     activeColor: Colors.red,
+      //   ),
+      // ),
