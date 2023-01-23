8262fa46f57a0a9bf400649aad8412d74b2ecde7
scawand <108281686+scawand@users.noreply.github.com>
2023-01-23 09:11:41 -0500

    avant pull main, sharedPrefence test

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/Home/home.dart';
 //import 'package:flutter_application_1/src/Home/home.dart';
+import 'package:flutter_application_1/src/Home/home.dart';
+//import 'package:flutter_application_1/src/Home/home.dart';
 import 'package:flutter_application_1/src/Pages/Business/business_screen.dart';
 import 'package:flutter_application_1/src/Pages/School/school_page.dart';
 
